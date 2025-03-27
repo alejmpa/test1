c936aae20b67b52d80034b2857e53b548d587ccf
alejmpa <amedp@hotmail.com>
2025-03-27 12:35:09 -0600

    Modificaciones 1

--- src/main/java/mx/tecnm/toluca/JakartaRestConfiguration.java
+++ src/main/java/mx/tecnm/toluca/JakartaRestConfiguration.java
@@ -7,7 +7,9 @@ import jakarta.ws.rs.core.Application;
  * Configures Jakarta RESTful Web Services for the application.
  * @author Juneau
  */
+//
+//
+//
 @ApplicationPath("resources")
-public class JakartaRestConfiguration extends Application {
-    
+public class JakartaRestConfiguration extends Application {  
 }
---
+++ src/main/java/mx/tecnm/toluca/MyClass.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mx.tecnm.toluca;
+
+/**
+ *
+ * @author Medina
+ */
+public class MyClass {
+    public static void main(String[] args) {
+        
+    }
+}
